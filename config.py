6b502b8565ff3f3b56dacc791a0fd2fbc11b8432
seongbae <pseongbae@gmail.com>
2025-06-02 01:24:20 +0900

    Formatting

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import torch
 
-TEST_DATA_PATH = 'data/zalando-hd-resized/test/'
-device = 'cuda'
+TEST_DATA_PATH = "data/zalando-hd-resized/test/"
+device = "cuda"
 concat_d = -2
-dtype = torch.bfloat16 if device == 'cuda' else torch.float32
-base_ckpt = 'stable-diffusion-v1-5/stable-diffusion-inpainting'
+dtype = torch.bfloat16 if device == "cuda" else torch.float32
+base_ckpt = "stable-diffusion-v1-5/stable-diffusion-inpainting"
