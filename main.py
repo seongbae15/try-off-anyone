16b003a77d98954d6f835e166dd473f54996cd16
seongbae <pseongbae@gmail.com>
2025-06-05 09:03:45 +0900

    dev train code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from src.test_vton import test_vton
 from src.inference import test_image
+from src.train import train
 import argparse
 
 
@@ -7,15 +8,16 @@ def terminal_args():
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
     parser.add_argument("--test", action="store_true")
     parser.add_argument("--inference", action="store_true")
+    parser.add_argument("--train", action="store_true")
     return parser.parse_known_args()[0]
 
 
 def main():
     args = terminal_args()
-    # if args.test:
-    #     test_vton()
-    # args.inference:
-    test_image()
+    if args.train:
+        train()
+    elif args.inference:
+        test_image()
 
 
 if __name__ == "__main__":
